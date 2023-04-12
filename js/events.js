a6d59027faa97d109263e210aea1c6ba14000f9a
fulong <fulonghuang05@gmail.com>
2023-04-12 01:30:41 -0700

    arrow key to control

--- js/events.js
+++ js/events.js
@@ -5,15 +5,19 @@ window.addEventListener('keyup', keyup)
 function keydown(event){
     switch(event.key){
         case 'a':
+        case 'ArrowLeft':
             gameboard.moveLeft()
             break;
         case 'd':
+        case 'ArrowRight':
             gameboard.moveRight()
             break;
         case 's':
+        case 'ArrowDown':
             gameboard.fastDrop()
             break;
         case 'w':
+        case 'ArrowUp':
             gameboard.rotate()
             break;
         case 'p':
@@ -31,6 +35,7 @@ function keydown(event){
 function keyup(event){
     switch(event.key){
         case 's':
+        case 'ArrowDown':
             gameboard.normalDrop()
             break;
     }
